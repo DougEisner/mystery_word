9f6df6a64b9ef029bb98e389fd88ed8d7a414ddc
Doug Eisner <doug.eisner@gmail.com>
2017-01-13 16:43:01 -0500

    Initial commit of ruby file.

---
+++ mystery_word.rb
@@ -0,0 +1,34 @@
+word_source = File.readlines('/usr/share/dict/words')
+
+words = word_source.map { |word| word.chomp.downcase }
+
+def create_word_groups(words)
+  list = []
+  words.each do |word|
+    if word.length == 4 or word.length == 5 or word.length == 6
+      list << word
+    end
+  end
+  list
+end
+
+def select_random_word(word_list)
+  word_list.sample
+end
+
+#Bryce's code to create list of 4-6 words
+# def create_word_groups(words)
+#   words.select do |word|
+#     word.length == 4 or word.length == 5 or word.length == 6
+#   end
+# end
+
+word_list = create_word_groups(words)
+
+#p word_list
+p word_list.count
+
+random_word = select_random_word(word_list)
+puts "random word: " + random_word
+
+puts "The length of your word is #{random_word.length}"
